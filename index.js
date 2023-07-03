bb15cc58cc032e24b8e3e97cbaa6c5c44e935002
Alex Colson <hello@ajcolson.com>
2023-07-03 09:39:32 -0400

    Updated Email

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e)=>{
   console.log("Hello, World!")
+  document.querySelector("#copyEmailToClipboardBtn").addEventListener("click",(e)=>{
+    navigator.clipboard.writeText("hello@ajcolson.com").then(()=>{},()=>{})
+  })
 })
