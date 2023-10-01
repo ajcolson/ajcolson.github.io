3b78b470aa5412d445dc90c2bb13fbb74cac3554
Alex Colson <hello@ajcolson.com>
2023-10-01 11:31:58 -0400

    Update index.js

--- res/js/index.js
+++ res/js/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", (e)=>{
 
 
   document.querySelector("#copyEmailToClipboardBtn").addEventListener("click",(e)=>{
-    navigator.clipboard.writeText("hello@ajcolson.com").then(()=>{},()=>{})
+    navigator.clipboard.writeText("alex@ajcolson.com").then(()=>{},()=>{})
   })
   
   document.querySelector("#darkThemeToggle").addEventListener("click",(e)=>{
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", (e)=>{
   })
 
   document.querySelector("#emailModal").addEventListener("show.bs.modal",function(){
-    document.querySelector("#emailAddrInput").value = "hello@ajcolson.com"
-    document.querySelector("#emailAddrLink").href = "mailto:hello@ajcolson.com"
+    document.querySelector("#emailAddrInput").value = "alex@ajcolson.com"
+    document.querySelector("#emailAddrLink").href = "mailto:alex@ajcolson.com"
   })
 
   document.querySelector("#emailModal").addEventListener("hidden.bs.modal",function(){
