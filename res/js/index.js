029bbdf9eccbdb43774892c1dbb781104bc44d90
Alex Colson <alex@ajcolson.com>
2024-06-08 15:31:25 -0400

    Update index.js

--- res/js/index.js
+++ res/js/index.js
@@ -22,8 +22,12 @@ document.addEventListener("DOMContentLoaded", (e)=>{
   })
 
   document.querySelector("#emailModal").addEventListener("show.bs.modal",function(){
-    document.querySelector("#emailAddrInput").value = "alex@ajcolson.com"
-    document.querySelector("#emailAddrLink").href = "mailto:alex@ajcolson.com"
+    document.querySelector("#emailAddrInput").value = "616c657840616a636f6c736f6e2e636f6d".match(/.{1,2}/g).map(function(v){
+      return String.fromCharCode(parseInt(v, 16));
+    }).join('')
+    document.querySelector("#emailAddrLink").href = "6d61696c746f3a616c657840616a636f6c736f6e2e636f6d".match(/.{1,2}/g).map(function(v){
+      return String.fromCharCode(parseInt(v, 16));
+    }).join('')
   })
 
   document.querySelector("#emailModal").addEventListener("hidden.bs.modal",function(){
