9c3a393890dc4993951c2f9babe78bafdfa2eb17
Alex Colson <alex@ajcolson.com>
2025-02-04 22:29:24 -0500

    Site Updates

--- res/js/index.js
+++ res/js/index.js
@@ -21,18 +21,4 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     __.Config.set("themeName", themeName)
   })
 
-  document.querySelector("#emailModal").addEventListener("show.bs.modal",function(){
-    document.querySelector("#emailAddrInput").value = "616c657840616a636f6c736f6e2e636f6d".match(/.{1,2}/g).map(function(v){
-      return String.fromCharCode(parseInt(v, 16));
-    }).join('')
-    document.querySelector("#emailAddrLink").href = "6d61696c746f3a616c657840616a636f6c736f6e2e636f6d".match(/.{1,2}/g).map(function(v){
-      return String.fromCharCode(parseInt(v, 16));
-    }).join('')
-  })
-
-  document.querySelector("#emailModal").addEventListener("hidden.bs.modal",function(){
-    document.querySelector("#emailAddrInput").value = ""
-    document.querySelector("#emailAddrLink").href = ""
-  })
-
 })
